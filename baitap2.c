ebdf4eb2c9499408e0f59b4c35ac3bb6f01220ee
Trong Ho <91108005+TrongHvDev@users.noreply.github.com>
2023-02-20 14:48:42 +0700

    Add files via upload

---
+++ baitap2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+	int a,b;
+	printf("Nhập vào hai số a và b");
+	scanf("%d, %d",&a,&b);
+	float c;
+		c = (a+b)/2.0;
+	printf("trung binh la: %f \n", c);
+	return 0;
+}
